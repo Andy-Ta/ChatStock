b622a2cb7a3ea4545c5c3227cd2aa434e91cc24c
Andy Ta <me.andyta@gmail.com>
2019-01-27 07:34:46 -0500

    Yeah Yeah

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,31 @@ from flask import Flask
 app = Flask(__name__)
 app.secret_key = 'gietmamaw'
 
-from app.routes import api
+import json
+import time
+
+from flask import request
+from app import app
+import pyttsx as pyttsx
+from googletrans import Translator
+
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    message = request.get_json(force=True)['message']
+    translator = Translator()
+    print("\nTranslation started...")
+    start_time = time.time()
+    translated = translator.translate(message, dest='fr').text
+    elapsed_time = time.time() - start_time
+    print("Time taken to translate: " + str(elapsed_time))
+    print(translated)
+
+    engine = pyttsx.init()
+    voices = engine.getProperty('voices')
+    for voice in voices:
+        print(voice)
+    engine.setProperty('voice', 'french')
+    engine.say(translated)
+    engine.runAndWait()
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
